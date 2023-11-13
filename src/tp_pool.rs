7a0caaf17e608b8da8b6b05081fee6c7bfe57107
Julian Schmid <info@julianschmid.name>
2023-11-13 21:09:36 +0100

    Add timestamp to tp pool

--- src/tp_pool.rs
+++ src/tp_pool.rs
@@ -14,10 +14,14 @@ use std::collections::HashMap;
 /// Mitigations will hopefully be offered in future versions but if you have
 /// take care right now you can still use [`TpBuf`] directly and implement the
 /// connection handling and mitigation yourself.
-#[derive(Debug, Clone, Eq, PartialEq)]
-pub struct TpPool<ChannelId: Hash + Eq + PartialEq + Clone + Sized> {
+#[derive(Debug, Clone)]
+pub struct TpPool<ChannelId, Timestamp>
+where
+    ChannelId: Hash + Eq + PartialEq + Clone + Sized,
+    Timestamp: Sized + core::fmt::Debug + Clone,
+{
     /// Currently reconstructing TP streams.
-    active: HashMap<(ChannelId, u32), TpBuf>,
+    active: HashMap<(ChannelId, u32), (TpBuf, Timestamp)>,
 
     /// Buffers that have finished receiving data and can be re-used.
     finished: Vec<TpBuf>,
@@ -26,8 +30,12 @@ pub struct TpPool<ChannelId: Hash + Eq + PartialEq + Clone + Sized> {
     buf_config: TpBufConfig,
 }
 
-impl<ChannelId: Hash + Eq + PartialEq + Clone + Sized> TpPool<ChannelId> {
-    pub fn new(buf_config: TpBufConfig) -> TpPool<ChannelId> {
+impl<ChannelId, Timestamp: Sized> TpPool<ChannelId, Timestamp>
+where
+    ChannelId: Hash + Eq + PartialEq + Clone + Sized,
+    Timestamp: core::fmt::Debug + Clone + Sized,
+{
+    pub fn new(buf_config: TpBufConfig) -> TpPool<ChannelId, Timestamp> {
         TpPool {
             active: HashMap::new(),
             finished: Vec::new(),
@@ -35,22 +43,47 @@ impl<ChannelId: Hash + Eq + PartialEq + Clone + Sized> TpPool<ChannelId> {
         }
     }
 
+    /// Reserves the given number as buffers.
+    pub fn reserve(&mut self, additional: usize) {
+        self.finished.reserve(additional);
+        for _ in 0..additional {
+            self.finished.push(TpBuf::new(self.buf_config.clone()));
+        }
+    }
+
+    #[inline]
+    pub fn active_bufs(&self) -> &HashMap<(ChannelId, u32), (TpBuf, Timestamp)> {
+        &self.active
+    }
+
+    #[inline]
+    pub fn finished_bufs(&self) -> &Vec<TpBuf> {
+        &self.finished
+    }
+
+    #[inline]
+    pub fn buf_config(&self) -> &TpBufConfig {
+        &self.buf_config
+    }
+
     pub fn consume<'a: 'c, 'b: 'c, 'c: 'a + 'b>(
         &'a mut self,
         id: ChannelId,
+        timestamp: Timestamp,
         someip_slice: SomeipMsgSlice<'b>,
     ) -> Result<Option<SomeipMsgSlice<'c>>, err::TpReassembleError> {
         if someip_slice.is_tp() {
             use std::collections::hash_map::Entry::*;
             match self.active.entry((id, someip_slice.request_id())) {
                 Occupied(mut o) => {
-                    // stream already known consume the data
-                    o.get_mut().consume_tp(someip_slice)?;
+                    // stream already known consume the data & update the timestamp
+                    o.get_mut().0.consume_tp(someip_slice)?;
+                    o.get_mut().1 = timestamp;
 
                     // check if the stream is complete
-                    if o.get().is_complete() {
+                    if o.get().0.is_complete() {
                         // if done move the buffer to the finished list and return the result
-                        self.finished.push(o.remove());
+                        self.finished.push(o.remove().0);
                         Ok(Some(
                             self.finished.last_mut().unwrap().try_finalize().unwrap(),
                         ))
@@ -79,7 +112,7 @@ impl<ChannelId: Hash + Eq + PartialEq + Clone + Sized> TpPool<ChannelId> {
                         ))
                     } else {
                         // stream is not yet done, keep it around until done
-                        v.insert(buf);
+                        v.insert((buf, timestamp));
                         Ok(None)
                     }
                 }
@@ -88,6 +121,47 @@ impl<ChannelId: Hash + Eq + PartialEq + Clone + Sized> TpPool<ChannelId> {
             Ok(Some(someip_slice))
         }
     }
+
+    /// Retains only the elements specified by the predicate.
+    pub fn retain<F>(&mut self, f: F)
+    where
+        F: Fn(&Timestamp) -> bool,
+    {
+        // check if any entry has to be removed
+        if self.active.iter().any(|(_, (_, t))| false == f(t)) {
+            self.active = self
+                .active
+                .drain()
+                .filter_map(|(k, v)| {
+                    if f(&v.1) {
+                        Some((k, v))
+                    } else {
+                        self.finished.push(v.0);
+                        None
+                    }
+                })
+                .collect();
+        }
+    }
+}
+
+impl<ChannelId, Timestamp: Sized> PartialEq for TpPool<ChannelId, Timestamp>
+where
+    ChannelId: Hash + Eq + PartialEq + Clone + Sized,
+    Timestamp: core::fmt::Debug + Clone + Sized + PartialEq,
+{
+    fn eq(&self, other: &Self) -> bool {
+        self.active == other.active
+            && self.finished == other.finished
+            && self.buf_config == other.buf_config
+    }
+}
+
+impl<ChannelId, Timestamp: Sized> Eq for TpPool<ChannelId, Timestamp>
+where
+    ChannelId: Hash + Eq + PartialEq + Clone + Sized,
+    Timestamp: core::fmt::Debug + Clone + Sized + PartialEq + Eq,
+{
 }
 
 #[cfg(test)]
@@ -96,9 +170,19 @@ mod tests {
 
     #[test]
     fn debug_clone_eq() {
-        let pool: TpPool<()> = TpPool::new(Default::default());
+        let pool: TpPool<(), ()> = TpPool::new(Default::default());
         let _ = format!("{:?}", pool);
         assert_eq!(pool, pool.clone());
+        assert_eq!(pool.buf_config(), &TpBufConfig::default());
+    }
+
+    #[test]
+    fn reserve() {
+        let mut pool = TpPool::<(), ()>::new(Default::default());
+        pool.reserve(2);
+        assert_eq!(2, pool.finished_bufs().len());
+        pool.reserve(3);
+        assert_eq!(5, pool.finished_bufs().len());
     }
 
     struct TestPacket {
@@ -185,31 +269,31 @@ mod tests {
             
             let someip_slice = SomeipMsgSlice::from_slice(&result).unwrap();
 
-            let mut pool: TpPool<()> = TpPool::new(TpBufConfig::new(1024, 2048).unwrap());
-            let result = pool.consume((), someip_slice.clone()).unwrap();
+            let mut pool: TpPool<(), ()> = TpPool::new(TpBufConfig::new(1024, 2048).unwrap());
+            let result = pool.consume((), (), someip_slice.clone()).unwrap();
             assert_eq!(Some(someip_slice), result);
         }
 
         // normal reconstruction (without additional id)
         {
-            let mut pool: TpPool<()> = TpPool::new(TpBufConfig::new(1024, 2048).unwrap());
+            let mut pool: TpPool<(), ()> = TpPool::new(TpBufConfig::new(1024, 2048).unwrap());
 
             let actions = [
                 // start two streams in parallel
-                (TestPacket::new(1, 0, true, &sequence(1,16)), None),
-                (TestPacket::new(2, 0, true, &sequence(2,32)), None),
+                (TestPacket::new(1, 0, true, &sequence(1,16)), None, 1, 0),
+                (TestPacket::new(2, 0, true, &sequence(2,32)), None, 2, 0),
                 // stream 1 ends
-                (TestPacket::new(1, 16, false, &sequence(1 + 16,16)), Some(sequence(1,32))),
+                (TestPacket::new(1, 16, false, &sequence(1 + 16,16)), Some(sequence(1,32)), 1, 1),
                 // stream 3 which imidiatly ends
-                (TestPacket::new(3, 0, false, &sequence(3,16*4)), Some(sequence(3, 16*4))),
+                (TestPacket::new(3, 0, false, &sequence(3,16*4)), Some(sequence(3, 16*4)), 1, 1),
                 // end stream 2
-                (TestPacket::new(2, 32, true, &sequence(32 + 2,16*4)), None),
-                (TestPacket::new(2, 16*6, false, &sequence(16*6 + 2,16*3)), Some(sequence(2, 16*9))),
+                (TestPacket::new(2, 32, true, &sequence(32 + 2,16*4)), None, 1, 1),
+                (TestPacket::new(2, 16*6, false, &sequence(16*6 + 2,16*3)), Some(sequence(2, 16*9)), 0, 2),
             ];
             for a in actions {
                 let packet = a.0.to_vec();
                 let slice = SomeipMsgSlice::from_slice(&packet).unwrap();
-                let result = pool.consume((), slice).unwrap();
+                let result = pool.consume((), (), slice).unwrap();
                 if let Some(expected_payload) = a.1 {
                     let msg = result.unwrap();
                     assert_eq!(msg.to_header(), a.0.result_header(expected_payload.len() as u32));
@@ -217,12 +301,14 @@ mod tests {
                 } else {
                     assert!(result.is_none());
                 }
+                assert_eq!(a.2, pool.active_bufs().len());
+                assert_eq!(a.3, pool.finished_bufs().len());
             }
         }
 
         // normal reconstruction (with additional id)
         {
-            let mut pool: TpPool<u32> = TpPool::new(TpBufConfig::new(1024, 2048).unwrap());
+            let mut pool: TpPool<u32, ()> = TpPool::new(TpBufConfig::new(1024, 2048).unwrap());
 
             // all actions have the same request id have differing id's
             let actions = [
@@ -240,7 +326,7 @@ mod tests {
             for a in actions {
                 let packet = a.1.to_vec();
                 let slice = SomeipMsgSlice::from_slice(&packet).unwrap();
-                let result = pool.consume(a.0.clone(), slice).unwrap();
+                let result = pool.consume(a.0.clone(), (), slice).unwrap();
                 if let Some(expected_payload) = a.2 {
                     let msg = result.unwrap();
                     assert_eq!(msg.to_header(), a.1.result_header(expected_payload.len() as u32));
@@ -253,35 +339,69 @@ mod tests {
 
         // error during reconstruction (at start)
         {
-            let mut pool: TpPool<()> = TpPool::new(TpBufConfig::new(1024, 2048).unwrap());
+            let mut pool: TpPool<(), ()> = TpPool::new(TpBufConfig::new(1024, 2048).unwrap());
 
             // should trigger an error as the payload is not a multiple of 1
             let packet = TestPacket::new(1, 0, true, &sequence(1,15)).to_vec();
             let someip_slice = SomeipMsgSlice::from_slice(&packet).unwrap();
             assert_eq!(
-                pool.consume((), someip_slice).unwrap_err(),
+                pool.consume((), (), someip_slice).unwrap_err(),
                 UnalignedTpPayloadLen { offset: 0, payload_len: 15 }
             );
         }
 
         // error during reconstruction (after start)
         {
-            let mut pool: TpPool<()> = TpPool::new(TpBufConfig::new(1024, 2048).unwrap());
+            let mut pool: TpPool<(), ()> = TpPool::new(TpBufConfig::new(1024, 2048).unwrap());
 
             {
                 let packet = TestPacket::new(1, 0, true, &sequence(1,16)).to_vec();
                 let someip_slice = SomeipMsgSlice::from_slice(&packet).unwrap();
-                pool.consume((), someip_slice).unwrap();
+                pool.consume((), (), someip_slice).unwrap();
             }
 
             // should trigger an error as the payload is not a multiple of 1
             let packet = TestPacket::new(1, 16, true, &sequence(1,15)).to_vec();
             let someip_slice = SomeipMsgSlice::from_slice(&packet).unwrap();
             assert_eq!(
-                pool.consume((), someip_slice).unwrap_err(),
+                pool.consume((), (), someip_slice).unwrap_err(),
                 UnalignedTpPayloadLen { offset: 16, payload_len: 15 }
             );
         }
 
+    }
+
+    #[test]
+    fn retain() {
+        let mut pool: TpPool<u16, u32> = TpPool::new(Default::default());
+        // request id 1, channel id 2, timestamp 123
+        {
+            let packet = TestPacket::new(1, 0, true, &sequence(1, 16)).to_vec();
+            let slice = SomeipMsgSlice::from_slice(&packet).unwrap();
+            let result = pool.consume(2u16, 123u32, slice).unwrap();
+            assert!(result.is_none());
+            assert_eq!(123, pool.active_bufs().get(&(2u16, 1u32)).unwrap().1);
+        }
+        // request id 1, channel id 2, timestamp 124
+        {
+            let packet = TestPacket::new(1, 16, true, &sequence(16, 16)).to_vec();
+            let slice = SomeipMsgSlice::from_slice(&packet).unwrap();
+            let result = pool.consume(2u16, 124u32, slice).unwrap();
+            assert!(result.is_none());
+            // check the timestamp was overwritten by the newer packet
+            assert_eq!(124, pool.active_bufs().get(&(2u16, 1u32)).unwrap().1);
+        }
+        // request id 1, channel id 3, timestamp 125
+        {
+            let packet = TestPacket::new(1, 16, true, &sequence(16, 16)).to_vec();
+            let slice = SomeipMsgSlice::from_slice(&packet).unwrap();
+            let result = pool.consume(3u16, 125u32, slice).unwrap();
+            assert!(result.is_none());
+        }
+
+        // discard streams with a timestamp smaller then 125
+        pool.retain(|timestamp| *timestamp >= 125);
+
+        assert_eq!(125, pool.active_bufs().get(&(3u16, 1u32)).unwrap().1);
     }
 }
